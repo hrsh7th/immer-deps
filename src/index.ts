f3a7518fb757536f9de51bd436be374167a68347
hrsh7th <hrsh7th@gmail.com>
2019-01-25 15:51:35 +0900

    Improve

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-import { IProduce, Patch, PatchListener } from 'immer';
+import { IProduce, Patch, PatchListener, isDraft } from 'immer';
 import { DeepPath, Prop } from './deep-path';
 
 export type Define<State> = <Paths extends Prop[] = Prop[]>(...paths: Paths) => Definer<State, Paths>;
 export type Definer<State, Paths extends Prop[]> = (resolver: Resolver<State, Paths>) => Definition<State, Paths>;
 export type Definition<State, Paths extends Prop[]> = { path: Prop[]; resolver: Resolver<State, Paths> };
 export type Resolver<State, Paths extends Prop[]> = (state: State, value: DeepPath<State, Paths>) => any[];
+
+type Context = {
+  patches: Patch[];
+  inversePatches: Patch[];
+};
 
 export function deps<State>(
   produce: IProduce,
@@ -20,13 +25,20 @@ export function deps<State>(
   //  });
 
   return (state: State, recipe: (state: State) => void, patchListener?: PatchListener) => {
-    const patches = [] as Patch[];
-    const inversePatches = [] as Patch[];
-    const current = produce(state, recipe, (patches_, inversePatches) => {
-      patches.push(...patches_);
-      inversePatches.push(...inversePatches);
+    if (isDraft(state)) {
+      return state;
+    }
+
+    const context: Context = {
+      patches: [],
+      inversePatches: []
+    };
+
+    const current = produce(state, recipe, (patches, inversePatches) => {
+      context.patches.push(...patches);
+      context.inversePatches.push(...inversePatches);
     });
-    patchListener && patchListener(patches, inversePatches)
+    patchListener && patchListener(context.patches, context.inversePatches)
     return current;
   };
 }
